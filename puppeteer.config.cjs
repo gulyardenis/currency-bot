558cf1f4484988d85b756635973018942fc8aea6
gulyardenis <gulyardenis@gmail.com>
2025-08-06 12:29:50 +0400

    Update puppeteer.config.cjs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,5 +1,18 @@
-// puppeteer.config.cjs
 const { join } = require('path');
+
 module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  defaultBrowserOptions: {
+    executablePath: join(
+      '/opt',
+      'render',
+      '.cache',
+      'puppeteer',
+      'chrome',
+      'linux-138.0.7204.168',
+      'chrome'
+    ),
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer')
 };
